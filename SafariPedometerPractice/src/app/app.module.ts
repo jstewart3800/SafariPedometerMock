f8226f46821c0e2f835542a1d97667cdb9f2246f
Jon Stewart <56611713+jstewart3800@users.noreply.github.com>
2020-03-11 21:47:41 -0700

    Attempted Pedo Functions

--- SafariPedometerPractice/src/app/app.module.ts
+++ SafariPedometerPractice/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Pedometer } from '@ionic-native/pedometer/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Pedometer,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- SafariPedometerPractice/src/app/home/home.page.ts
+++ SafariPedometerPractice/src/app/home/home.page.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
-import { Pedometer } from '@ionic-native/pedometer/ngx';
+import { Pedometer, IPedometerData } from '@ionic-native/pedometer/ngx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+     selector: 'app-home',
+     templateUrl: 'home.page.html',
+     styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor(public pedometer: Pedometer) {}
+     successHandler = function (pedometerData) {
+          // pedometerData.startDate; -> ms since 1970
+          // pedometerData.endDate; -> ms since 1970
+          pedometerData.numberOfSteps;
+          // pedometerData.distance;
+          // pedometerData.floorsAscended;
+          // pedometerData.floorsDescended;
+     };
 
-  
+     constructor(public pedometer: Pedometer) { }
+
+     pedo2() {
+          Pedometer.startPedometerUpdates(this.successHandler, onerror)
+               .subscribe((data: IPedometerData) => {
+                    console.log(data);
+               });
+     }
+
 
 }
